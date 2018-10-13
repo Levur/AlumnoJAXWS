7eb16e31cda77f0546447d912ef4210da9cfd8fc
Levur <levent_x13@hotmail.com>
2018-10-13 14:25:29 -0500

    firts commit

---
+++ src/main/java/org/certificatic/dao/AlumnoDAO.java
@@ -0,0 +1,17 @@
+package org.certificatic.dao;
+
+import java.util.List;
+
+import org.certificatic.entity.Alumno;
+
+public interface AlumnoDAO {
+	
+	Alumno guardarAlumno(Alumno alumno);
+	
+	List<Alumno> obtenerAlumnos();
+	
+	
+	
+	
+
+}
